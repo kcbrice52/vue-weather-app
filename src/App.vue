94c66a3e9094a2101464ed9f0556a45f7fc5e858
Kendall Brice <kcbrice15@gmail.com>
2021-11-07 10:20:14 -0500

    added daily and hourly forecast,  weather icons

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,45 @@
       </div>
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }}, {{weather.sys.country }}</div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
+          <div class="location">{{ weather.name }}, {{weather.sys.country }}</div>
           <div class="temp">{{ Math.round(weather.main.temp) }}°F</div>
+          <br>
+          <img v-bind:src="'http://openweathermap.org/img/wn/' + weather.weather[0].icon + '@2x.png'">
           <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+        <div class="forecast-box">
+          <template v-if="forecastView === 'hourly'">
+          <button class="button floated-right" v-on:click="viewDailyForecast">View Daily</button>
+          <h2>Hourly Forecast</h2>
+          <br>
+          <div class="row">
+            <div class="column-2" v-for="hour in 24" :key="hour">
+              <div class="hour-box">
+                <div class="hour">{{ moment(hourlyForecast[hour-1].dt*1000).format('h A')}}</div>
+                <div class="">{{ hourlyForecast[hour-1].temp }}&#176;F</div>
+              </div>
+            </div>
+          </div>
+          </template>
+          <template v-if="forecastView === 'daily'">
+          <button class="button floated-right" v-on:click="viewHourlyForecast">View Hourly</button>
+          <h2>Daily Forecast</h2>
+          <br>
+          <div class="row">
+            <div class="column" v-for="day in dailyForecast.list" :key="day.dt">
+              <div class="day-box">
+                <div class="day"> {{ moment(day.dt*1000).format('DD ddd') }} </div>
+                <div class="temp">{{  Math.round(day.temp.day) }}&#176;F</div>
+                <div class="temp">{{  Math.round(day.temp.night) }}&#176;F</div>
+                <img v-bind:src="'http://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png'">
+                <div>{{ day.weather[0].main }}</div>
+              </div>
+            </div>
+          </div>
+          </template>
         </div>
       </div>
     </main>
@@ -25,6 +58,7 @@
 </template>
 
 <script>
+import moment from 'moment';
 
 export default {
   name: 'App',
@@ -33,21 +67,53 @@ export default {
       api_key: '2209bb742e00865106d7950710ecf8ca',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
-      weather: {}
+      weather: {},
+      dailyForecast: {},
+      hourlyForecast: {},
+      forecastView: 'daily'
     }
   },
   methods: {
     fetchWeather (e) {
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=imperial&APPID=${this.api_key}`)
-          .then(response => {
-            return response.json();
-          }).then(this.setResults);
+        this.fetchCurrentWeather();
+        this.fetchDailyForecast();
+        // this.fetchHourlyForecast();
       }
     },
-    setResults (results) {
-      this.weather = results;
+    fetchCurrentWeather () {
+      fetch(`${this.url_base}weather?q=${this.query}&units=imperial&APPID=${this.api_key}`)
+        .then(response => {
+          return response.json();
+        }).then(this.setCurrentWeather);
     },
+    fetchDailyForecast () {
+      fetch(`${this.url_base}forecast/daily?q=${this.query}&cnt=6&units=imperial&appid=${this.api_key}`)
+        .then(response => {
+          return response.json();
+        }).then(this.setDailyForecast);
+    },
+    fetchHourlyForecast () {
+      fetch(`${this.url_base}forecast/hourly?q=${this.query}&units=imperial&appid=${this.api_key}`)
+        .then(response => {
+          return response.json();
+        }).then(this.setDailyForecast);
+    },
+    setCurrentWeather (results) {
+      this.weather = results;
+      console.log('test');
+      fetch(`${this.url_base}onecall?lat=${this.weather.coord.lat}&lon=${this.weather.coord.lon}&units=imperial&appid=${this.api_key}`)
+        .then(response => {
+          return response.json();
+        }).then(this.setHourlyForecast);
+    },
+    setDailyForecast (results) {
+      this.dailyForecast = results;
+    },
+    setHourlyForecast (results) {
+      this.hourlyForecast = results.hourly
+    },
+    moment,
     dateBuilder () {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -59,111 +125,13 @@ export default {
       let year = d.getFullYear();
 
       return `${day} ${date} ${month} ${year}`;
+    },
+    viewHourlyForecast () {
+      this.forecastView = 'hourly';
+    },
+    viewDailyForecast () {
+      this.forecastView = 'daily';
     }
   }
 }
 </script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: 'montserrat', sans-serif;
-}
-
-#app {
-  background-image: url('./assets/sky-bg.jpg');
-  background-size: center;
-  background-position: center;
-  transition: 0.4s;
-}
-
-#app.warm {
-  background-image: url('./assets/warm-bg.jpg');
-}
-
-#app.cold {
-  background-image: url('./assets/cold-bg.jpg');
-}
-
-
-main {
-  min-height: 100vh;
-  padding: 25px;
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
-}
-
-.search-box {
-  width: 100%;
-  margin-bottom: 30px;
-}
-
-.search-box .search-bar {
-  display: block;
-  width: 100%;
-  padding: 15px;
-
-  color: #313131;
-  font-size: 20px;
-  
-  appearance: none;
-  border: none;
-  outline: none;
-  background: none;
-  box-shadow: 0px 0px 8px rgba(255,255,255, 0.25);
-  background-color: rgba(255,255,255, 0.5);
-  border-radius: 0px 16px 0px 16px;
-  transition: 0.4s;
-}
-
-.search-box .search-bar:focus {
-  box-shadow: 0px 0px 16px rgba(255,255,255, 0.25);
-  background-color: rgba(255,255,255, 0.75);
-  border-radius: 16px 0px 16px 0px;
-}
-
-.location-box .location {
-  color: #fff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 1px 3px rgba(0,0,0,0.25);
-}
-
-.location-box .date {
-  color: #fff;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
-}
-
-.weather-box {
-  text-align: center;
-}
-
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
-  font-weight: 900;
-  text-shadow: 3px 6px rgba(0,0,0,0.25);
-  background-color: rgba(255,255,255, 0.25);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0,0,0,0.25);
-}
-
-.weather-box .weather {
-  color: #fff;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0,0,0,0.25);
-}
-</style>
